4ed9a35de0e04bc613d062f5bd2e5553b7562963
jay-toon <jaytoon.dev@gmail.com>
2024-04-18 14:29:47 +0100

    altering code and design. added ide

--- theme/switch_styles.js
+++ theme/switch_styles.js
@@ -9,14 +9,15 @@ document.addEventListener('DOMContentLoaded', function() { // Once the webpage h
 function switchStyles() {
     // Find the <link> element with rel="stylesheet" as the identifier
     const fetchStyles = document.querySelectorAll('link[rel="stylesheet"]');
-    let currentStyles = fetchStyles[1].getAttribute('href');
+    // Fetches the second value, as this will contain the theme's value
+    let currentStyles = fetchStyles[1].getAttribute('href'); 
     // If the stylesheet contains the value "light"...
     if (currentStyles.includes('light')) {
         // Replaces 'light' with 'dark' in the href value, switching to light mode
         fetchStyles[1].setAttribute('href', currentStyles.replace('light', 'dark'));
         localStorage.setItem("theme", "dark");
     } else { // Else, replace 'dark' with light in the href value, which switches it to dark mode
-        fetchStyles[1].setAttribute('href', currentStyles.replace('dark', 'light'));
+        fetchStyles[1].setAttribute('href', currentStyles.replace('dark', 'light'));    
         localStorage.setItem("theme", "light");
     }
 }
